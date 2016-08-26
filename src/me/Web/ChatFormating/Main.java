a79371daf7d0722b2d4c209f8f353383b84b456a
BuildTools <unconfigured@null.spigotmc.org>
2016-08-26 09:55:06 +0300

    Start of the plugin

---
+++ src/me/Web/ChatFormating/Main.java
@@ -0,0 +1,19 @@
+package me.Web.ChatFormating;
+
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        return false;
+    }
+}
